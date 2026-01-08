9b0b48ebb26bf21fe8a268571a0b4a03b918f6cd
Chrustho <christianbruni131@gmail.com>
2026-01-08 23:02:51 +0100

    + created main function to create a colored window

--- Main.cpp
+++ Main.cpp
@@ -9,7 +9,7 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-int main()
+int primaFinestra()
 {
 
     glfwInit();
@@ -43,6 +43,42 @@ int main()
 
     glfwTerminate();
     return 0;
+}
+
+int main() {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    gladLoadGL();
+
+    glViewport(0, 0, 800, 800);
+
+    glClearColor(0.50f, 0.50f, 0.50f, 0.50f);
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+
 }
 
 void processInput(GLFWwindow* window)
